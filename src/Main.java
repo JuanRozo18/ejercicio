53891d71798bdae1f1832599ad4658d70c43cbde
Juan Rozo <jucarope2006@gmail.com>
2024-09-30 11:06:35 -0500

    datos

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Juan Camilo Rozo Penagos
+        //juan.camilo-p@uniminuto.edu.co
+        //Cogua
+        //Escuchar musica
+
+    }
+}
